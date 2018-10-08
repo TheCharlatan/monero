b4782789ae6e95e071fe2a6f4d6eba66fe527021
TheCharlatan <seb.kung@gmail.com>
2018-10-08 12:42:11 +0200

    Add whitespace patch to iconv

--- contrib/depends/packages/libiconv.mk
+++ contrib/depends/packages/libiconv.mk
@@ -3,6 +3,7 @@ $(package)_version=1.15
 $(package)_download_path=https://ftp.gnu.org/gnu/libiconv
 $(package)_file_name=libiconv-$($(package)_version).tar.gz
 $(package)_sha256_hash=ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178
+$(package)_patches=fix-whitespace.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-nls 
@@ -12,7 +13,8 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux/
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux/ &&\
+  patch -p1 < $($(package)_patch_dir)/fix-whitespace.patch
 endef
 
 define $(package)_config_cmds
